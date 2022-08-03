281b84200641334fb267d0fb517485e8e7165743
morit <moritzhecht@gmx.net>
2022-08-03 14:35:05 +0200

    adding StyledButton

---
+++ component/StyledButton.tsx
@@ -0,0 +1,37 @@
+import {Pressable, StyleSheet, Text} from "react-native";
+import {COLOR_BACKGROUND, COLOR_HEADER} from "../constants/color_styles";
+import {useState} from "react";
+import {vh} from "../functions/dimentions";
+import {BORDER_RADIUS, BUTTON_HEIGHT, MARGIN, PADDING} from "../constants/style_constants";
+
+export default function StyledButton(props: any) {
+    const [isTouched, setIsTouched] = useState(false)
+    const onTouchStartHandler = () => {
+        setIsTouched(true)
+    }
+
+    const onTouchEndHandler = () => {
+        setIsTouched(false)
+    }
+
+    return (
+        <Pressable style={[styles.styledButton, {backgroundColor: isTouched ? COLOR_HEADER : COLOR_BACKGROUND}]}
+                   onTouchStart={onTouchStartHandler}
+                   onTouchEnd={onTouchEndHandler}
+                   onPress={props.onPress}>
+            <Text style={{textAlign: "center"}}>{props.title}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    styledButton: {
+        height: vh(BUTTON_HEIGHT),
+        borderRadius: BORDER_RADIUS / 2,
+        backgroundColor: COLOR_BACKGROUND,
+        padding: PADDING,
+        margin: MARGIN,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
