efad28255556d9ccac5a3330588c4c4de249a7b7
morit <moritzhecht@gmx.net>
2022-08-14 12:42:45 +0200

    -commented out status bar for now

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import {Provider as ReduxProvider} from "react-redux";
 import store from "./store/configureStore";
 import Routes from "./component/Routes";
+import {StatusBar} from "expo-status-bar";
 
 export default function App() {
     return (
         <ReduxProvider store={store}>
+            {/*<StatusBar style="auto"/>*/}
             <Routes/>
         </ReduxProvider>
     )
--- component/layout/AppBackground.tsx
+++ component/layout/AppBackground.tsx
@@ -1,9 +1,11 @@
 import {COLOR_BACKGROUND} from "../../constants/color_styles";
 import {Image, StyleSheet, View} from "react-native";
+import {StatusBar} from "expo-status-bar";
 
 export default function AppBackground(props: any) {
     return (
         <View style={styles.appBackground}>
+            {/*<StatusBar style="auto"/>*/}
             <Image style={styles.image}
                    source={require('../../assets/images/adaptive_background.png')}/>
             {props.children}
